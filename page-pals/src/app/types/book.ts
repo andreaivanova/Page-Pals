62aac06466375bcc4656e81930ada9e060703ce6
Andrea Ivanova <aiadea00@gmail.com>
2024-03-12 17:16:48 +0200

    Create User Model

---
+++ page-pals/src/app/types/book.ts
@@ -0,0 +1,13 @@
+
+
+
+// export interface Book {
+//     subscribers: string[];
+//     posts: any; //string[] | Post[];
+//     _id: string;
+//     name: string;
+//     userId: UserId;
+//     created_at: string;
+//     updatedAt: string;
+//     __v: number;
+// }
