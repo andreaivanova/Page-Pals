66889aa6227e9cdba613ce8e179e0a2dfdf23071
Andrea Ivanova <aiadea00@gmail.com>
2024-03-12 13:31:33 +0200

    Adding routes for login, register, home and adding HTML content to Register and Login components

--- page-pals/src/app/app-routing.module.ts
+++ page-pals/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './user/register/register.component';
+import { LoginComponent } from './user/login/login.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: '/home'
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  // {
+  //   path: '404', component: NotFoundComponent
+  // },
+  {path: '**', redirectTo: '404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
