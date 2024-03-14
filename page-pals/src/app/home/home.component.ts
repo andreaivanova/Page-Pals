2d473b602489eb4a47b55f60c263d6eb71ebf036
Andrea Ivanova <aiadea00@gmail.com>
2024-03-14 13:47:55 +0200

    Loading latest content on the home page

--- page-pals/src/app/home/home.component.ts
+++ page-pals/src/app/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../book/book.service';
+import { Book } from '../types/book';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit{
+
+  bookList: Book[]= [];
+  constructor(private bookService: BookService){
+    
+  }
+
+  ngOnInit(): void {
+    this.bookService.getThemes().subscribe(  {
+      next: (books)=>{
+        
+        this.bookList = books.slice(0,3);
+        console.log(this.bookList);
+      } ,
+      error: (err) =>{
+        console.error(`error: ${err}`)
+      }, 
+    });
+
+   
+  }
 }
