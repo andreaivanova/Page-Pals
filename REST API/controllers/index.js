a990fa6d1f9b8885436e482cdee7414ba6ab5e2c
Andrea Ivanova <aiadea00@gmail.com>
2024-03-18 01:55:28 +0200

    deleted rest api

--- REST API/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const authController = require('./auth');
-const bookController = require('./bookController');
-const postController = require('./postController');
-
-module.exports = {
-    authController,
-    bookController,
-    postController,
-}
